8208f03f822f5ef6ae71a35b0b0fb4e8b05d17f7
IGI-111 <igi.222@gmail.com>
2015-04-03 23:12:35 +0200

    added arguments

--- Main.hs
+++ Main.hs
@@ -3,11 +3,15 @@ module Main where
 import CSVParser
 import Apriori
 import qualified Data.Set as Set
+import System.Environment (getArgs)
 
 main :: IO()
 main = do
-	file <- readFile "out.csv"
+	args <- getArgs
+	let filename = head args
+	let threshold = read $ last args
+	file <- readFile filename
 	case parseCSV file of
 		Left _ -> putStrLn "Could not parse out.csv"
-		Right val -> mapM_ (\x -> putStrLn (show x ++ "(" ++ show (count table x) ++ ")")) (concat (frequentPatterns 0.6 table)) where
+		Right val -> mapM_ (\x -> putStrLn (show x ++ "(" ++ show (count table x) ++ ")")) (concat (frequentPatterns threshold table)) where
 			table = map (ItemSet. Set.fromList .map Item) val
